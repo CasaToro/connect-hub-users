44788f33f7f6427c016541ae8b0ef9a84b4864f3
JohnAragon <j-aragon@hotmail.com>
2020-07-08 14:41:52 -0500

    set token hub de usuarios

--- src/Http/Controllers/Auth/AuthController.php
+++ src/Http/Controllers/Auth/AuthController.php
@@ -3,12 +3,25 @@
 namespace Bellpi\ConnectHubUsers\Http\Controllers\Auth;
 use Bellpi\ConnectHubUsers\Utilities\Helpers;
 use Bellpi\ConnectHubUsers\Facades\HubConnection;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Http\Request;
 
 class AuthController {	  
+
+    public function checkToken($data){
+      $route= config('hub-paths.base').config('hub-paths.group');
+      $response=Helpers::httpPostJsonWithOutToken($route.'checkToken/'.$service_key,$data);
+      return $response;
+    }
+
   	public function login($service_key, $profile_key, $data){
       $route= config('hub-paths.base').config('hub-paths.group').config('hub-paths.path_login');
   		$response=Helpers::httpPostJsonWithOutToken($route.$service_key.'/'.$profile_key,$data);
+      $_response=json_decode($response);
+     
+      if($_response->data->user){
+         Config::set('token_hub_user',$_response->data->user->access_token);
+      }
   		return $response;
   	}
 
