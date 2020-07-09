ebb47fde81c72907910183e8225c78ab7148f11a
JohnAragon <j-aragon@hotmail.com>
2020-07-09 11:22:08 -0500

    Actualizacion rutas crear usuario

--- src/Http/Controllers/Auth/AuthController.php
+++ src/Http/Controllers/Auth/AuthController.php
@@ -17,7 +17,7 @@ class AuthController {
   	public function login($service_key, $profile_key, $data){
       $route= config('hub-paths.base').config('hub-paths.group').config('hub-paths.path_login');
   		$response=Helpers::httpPostJsonWithOutToken($route.$service_key.'/'.$profile_key,$data);
-      $_response=json_decode($response);
+      //$_response=json_decode($response);
      
       // if($_response->data->user){
       //    Config::set('token_hub_user',$_response->data->user->access_token);
@@ -27,12 +27,13 @@ class AuthController {
 
     public function getProfilesService($service_key, $data){
       $route= config('hub-paths.base').config('hub-paths.group').config('hub-paths.path_profiles');
-      $response=Helpers::httpPostJson('http://hub_users.test/api/v1/client/list-profiles/'.$service_key,$data);
+      $response=Helpers::httpPostJson($route.$service_key,$data);
       return $response;
     }
 
     public function userUpdate($key,$data){ 
-       $response=Helpers::httpPostJson('http://hub_users.test/api/v1/client/user/update/'.$key,$data);
+       $route= config('hub-paths.base').config('hub-paths.group').config('hub-paths.path_user_create');
+       $response=Helpers::httpPostJson($route.$key,$data);
        return $response;
     }
 }
